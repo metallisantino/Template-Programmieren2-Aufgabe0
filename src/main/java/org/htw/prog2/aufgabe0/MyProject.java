120ea9171137cddd2ea92a5d235298acd657b0aa
metallisantino <metallisantino@gmail.com>
2026-04-23 22:17:01 +0200

    fertige Aufgabe 2.0

--- src/main/java/org/htw/prog2/aufgabe0/MyProject.java
+++ src/main/java/org/htw/prog2/aufgabe0/MyProject.java
@@ -3,6 +3,7 @@ package org.htw.prog2.aufgabe0;
 import org.knowm.xchart.XYChart;
 import org.knowm.xchart.SwingWrapper;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class MyProject {
@@ -22,32 +23,25 @@ public class MyProject {
      */
     public static double[] calculateBabylonianRoot(double value, double initial, double maxerror) {
         // TODO: Implementieren.
-        // Schritt 1: Eine Liste für die Iterationen erstellen
+        // 1. X(n) und e(n) erstellen.
+        double xn = initial;
+        double en;
+        // 2. Liste für die Iterationen erstellen.
         java.util.LinkedList<Double> iterationen = new java.util.LinkedList<>();
-        // Schritt 2: Den Startwert in die Liste einfügen
-        iterationen.add(initial);
-        // Schritt 3: Eine Variable für die aktuelle Näherung (beginnt mit initial)
-        double aktuell = initial;
-        // Schritt 4: Schleife: wir rechnen, bis der Fehler klein genug is (while-Schleife, die wir bei Bedarf abbrechen)
-        while(true) {
-            // Fehler berechnen: (value - X2) / (2 * X)
-            double fehler = (value - (aktuell * aktuell)) / (2 * aktuell);
-            // Neue Näherung: X = X + Fehler
-            double neu = aktuell + fehler;
-            // Neue Näherung in die Liste einfügen
-            iterationen.add(neu);
-            if (Math.abs(fehler) <= maxerror){
-                break; // Genauigkeit erreicht: Schleife beenden. Wenn |Fehler| <= maxerror, dann Schleife abbrechen
-            }
-            aktuell = neu;
-        }
-
-        // Schritt 6: Liste in double[] umwandeln und zurückgeben
+        // 3. Startwert in Liste einfügen.
+        iterationen.add(xn);
+        // 4. do-while-Schleife: wir rechnen while |Fehler| > maxerror
+        do {
+            // e(n)=(S-X(n-1)²)/(2*X(n-1))
+            en = (value-Math.pow(xn,2))/(2*xn);
+            // X(n)=X(n-1)+e(n)
+            xn=xn+en;
+            // Neue Näherung in die Liste einfügen.
+            iterationen.add(xn);
+        } while (Math.abs(en) > maxerror); // Abbruch bei |Fehler| <= maxerror
+        // 5. Liste in double[] umwandeln und zurückgeben.
         double[] ergebnis = new double[iterationen.size()];
-        for (int i=0; i < iterationen.size(); i++) {
-            ergebnis[i] = iterationen.get(i);
-
-        }
+        for (int i=0; i < iterationen.size(); i++) {ergebnis[i] = iterationen.get(i);}
         return ergebnis;
     }
 
